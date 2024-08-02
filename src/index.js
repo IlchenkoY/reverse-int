dd33efbc1707f11abf6360a097dda0499023b854
Yaroslav Ilchencko <ilchenckoyaroslav@gmail.com>
2024-08-02 20:46:36 +0300

    feat: implement reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    const stringifyNum = Math.abs(n).toString();
+    let reverseInt = "";
+    for (let i = stringifyNum.length - 1; i >= 0; i -= 1) {
+        reverseInt += stringifyNum[i];
+    }
+    return Number(reverseInt);
+};
